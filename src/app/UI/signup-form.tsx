cc12cb3e29d97c0487ce05e787f720515deaf186
Bidbogs-prog <haythamchhilif@gmail.com>
2025-01-08 23:52:34 +0100

    added supabase configuration need to set up DB log

--- src/app/UI/signup-form.tsx
+++ src/app/UI/signup-form.tsx
@@ -1,8 +1,35 @@
 import { useActionState } from "react";
 import { signup } from "../actions/auth";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+import { createClient } from "@/utils/supabase/component";
 
 export function SignupForm() {
   const [state, action, pending] = useActionState(signup, undefined);
+  const router = useRouter();
+  const supabase = createClient();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function logIn() {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) {
+      console.error(error);
+    }
+    router.push("/");
+  }
+
+  async function signUp() {
+    const { error } = await supabase.auth.signUp({ email, password });
+    if (error) {
+      console.error(error);
+    }
+    router.push("/");
+  }
 
   return (
     <form
